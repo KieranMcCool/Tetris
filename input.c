e98e3d6d6bb653436b799a485c0642c9cd094168
Kieran McCool <kieranmccool332@gmail.com>
2020-07-28 02:33:22 +0100

    created skeleton input code for handling user input

---
+++ input.c
@@ -0,0 +1,38 @@
+#include "input.h"
+
+Action Input_PollEvent()
+{
+    Action action = NONE;
+    SDL_Event e;
+    if (SDL_PollEvent(&e) != 0)
+    {
+        if (e.type == SDL_QUIT)
+        {
+            action = QUIT;
+        }
+        else if (e.type == SDL_KEYDOWN)
+        {
+            switch (e.key.keysym.sym)
+            {
+            case 'w':
+                action = UP;
+                break;
+            case 's':
+                action = DOWN;
+                break;
+            case 'a':
+                action = LEFT;
+                break;
+            case 'd':
+                action = RIGHT; 
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    return action;
+}
+
+
